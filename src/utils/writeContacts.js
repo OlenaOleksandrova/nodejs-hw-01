b62fedbc587af34da81c1f0dc52682f9e6824be2
Olena Oleksandrova <ha240886kev@gmail.com>
2025-01-11 00:25:02 +0200

    rework

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -4,7 +4,7 @@ import fs from 'fs/promises';
 export const writeContacts = async (updatedContacts) => {
     try {
         const jsonFile = JSON.stringify(updatedContacts, null, 2);
-        await fs.writeFile(PATH_DB, 'utf8',jsonFile);
+        await fs.writeFile(PATH_DB, jsonFile, 'utf8');
         console.log('Дані записані у файл');
     } catch (err) {
         console.log('Помилка:', err);
